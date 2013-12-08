5ba1112cb461a5dc60b0fef8afd9f971821f23e9
lingmonster <swr@outlook.com>
2013-12-08 00:42:50 -0500

    Main

---
+++ about.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <?php include 'head.php'; ?>
+    <body id="about">
+        <?php include 'header.php'; ?>
+        <?php include 'nav.php'; ?>
+        <h1>Learn <i>About</i> Us</h1>
+        <!--<p>Veridian Dynamics is a company built on money, and people.</p>-->
+        <article class="card center">
+            <section class="third">
+                <p>
+                    <i>Veridian Dynamics is a family, just like yours.</i>
+                    <br/><br/>
+                    But we don't 
+                    waste our time with game night. We put our family to 
+                    work. We mean real work. Not just raking the yard.   
+                </p>
+            </section>
+            <section class="third">
+                <p>
+                    <i>Our family works for every member of your 
+                    family.</i> 
+                    <br/><br/>
+                    Even the 
+                    dead ones. And we're working to bring them back and copy them, 
+                    in case you lose them again. 
+                </p>
+            </section>
+            <section class="third">
+                <p>
+                    <i>We love our Veridian Dynamic family.</i> 
+                    <br/><br/>
+                    Which is why we
+                    work nights, weekends, and major holidays. Because that's when 
+                    families should be together.
+                </p>
+            </section>
+        </article>
+        <img class="resize" src="image/pic3a.png" alt='Veridian is a family.' height='729' width='900'>
+<!--        <article class="card" id="family">
+            <section>
+            <h2>Family</h2>
+            
+            <a href="http://www.flickr.com/photos/safari_vacation/7496765660/">SalFalko</a> via <a href="http://photopin.com">photopin</a> <a href="http://creativecommons.org/licenses/by-nc/2.0/">cc</a>
+            <p><i>Veridian Dynamics is a family, just like yours.</i>
+                <br/><br/>
+                But we don't 
+                waste our time with game night. We put our family to 
+                work. We mean real work. Not just raking the yard.
+                <br/><br/>
+                <i>Our Veridian Dynamic family works for every member of your 
+                    family.</i> 
+                <br/><br/>
+                Even the 
+                dead ones. And we're working to bring them back and copy them, 
+                in case you lose them again. 
+                <br/><br/>
+                <i>We love our family.</i> 
+                <br/><br/>
+                Which is why we
+                work nights, weekends, and major holidays. Because that's when 
+                families should be together.</p>
+            </section>
+            <section>
+                <img class="resize" src="image/pic3a.png" alt='Veridian is a family.' height='729' width='900'>
+                <a href="apply.php"><i>Join the Veridian Dynamics family today.</i></a>
+            </section>
+        </article>-->
+        
+
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
